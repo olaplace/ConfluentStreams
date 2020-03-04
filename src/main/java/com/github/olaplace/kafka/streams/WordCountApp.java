ada982bcc3a9186e98240b3e3bea4a1cc1954075
olaplace <olaplace@confluent.io>
2020-03-04 10:52:51 +0100

    Added JUnit tests for Word Count stream application

--- src/main/java/com/github/olaplace/kafka/streams/WordCountApp.java
+++ src/main/java/com/github/olaplace/kafka/streams/WordCountApp.java
@@ -6,23 +6,14 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Named;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
 
 import java.util.Arrays;
 import java.util.Properties;
 
 public class WordCountApp {
-    public static void main(String[] args) {
-        Properties config = new Properties();
-        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-application");
-        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
+    public Topology createTopology(){
         StreamsBuilder builder = new StreamsBuilder();
 
         // 1 - Stream from Kafka's topic
@@ -33,10 +24,10 @@ public class WordCountApp {
         KTable <String, Long> wordCounts =  wordCountInput
                 // 2 - map values in lower case
                 .mapValues(textLine -> textLine.toLowerCase())
-                        // Can be alternatively written as
-                        // .mapValues(String::toLowerCase)
+                // Can be alternatively written as
+                // .mapValues(String::toLowerCase)
                 // 3 - Flat map values by space
-                .flatMapValues(lowercaseTextLine -> Arrays.asList(lowercaseTextLine.split(" ")))
+                .flatMapValues(lowercaseTextLine -> Arrays.asList(lowercaseTextLine.split("\\W+")))
                 // 4 - Select Key to apply a key (we want to discard the old key)
                 //.selectKey((ignoreKey, word) -> word)
                 .groupBy((ignoreKey, word) -> word)
@@ -46,26 +37,48 @@ public class WordCountApp {
                 //.count(Named.as("Counts"));
                 .count();
 
-                // 7 - Write the data back to Kafka
-                //wordCounts.toStream().to( "word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
-                wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
+        // 7 - Write the data back to Kafka
+        //wordCounts.toStream().to( "word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
+        wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
 
-        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        return builder.build();
+    }
+
+    public static void main(String[] args) {
+
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-application");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        WordCountApp wordCountApp = new WordCountApp();
+
+        KafkaStreams streams = new KafkaStreams(wordCountApp.createTopology(), config);
 
         // The drawback of cleaning up local state prior is that your app must rebuilt its local state from scratch, which
         // will take time and will require reading all the state-relevant data from the Kafka cluster over the network.
         // Thus in a production scenario you typically do not want to clean up always as we do here but rather only when it
         // is truly needed, i.e., only under certain conditions (e.g., the presence of a command line flag for your app).
         // See `ApplicationResetExample.java` for a production-like example.
-        streams.cleanUp();
+        //streams.cleanUp();
         streams.start();
 
         // Print the topology
-        System.out.println(streams.toString());
+        //System.out.println(streams.toString());
+        // Update:
+        // print the topology every 5 seconds for learning purposes
+        while(true){
+            streams.localThreadsMetadata().forEach(data -> System.out.println(data));
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
 
         // Add a shutdown hook to close properly the streams application
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-
-
     }
 }
